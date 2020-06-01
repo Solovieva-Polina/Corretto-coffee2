4303cfbc73973a415dbb90c4ac83b41d756dcbd0
Solovieva-Polina <59253550+Solovieva-Polina@users.noreply.github.com>
2020-06-01 14:36:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*global $ */
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 10000,
+    fade: true,
+    prevArrow: '<div class="slider-btn btn-prev"></div>',
+    nextArrow: '<div class="slider-btn btn-next"></div>'
+});
